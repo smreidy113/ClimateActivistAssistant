7971a7065d7b00bdc757ea218d8eb3486f9c5d7d
Sean Reidy <smreidy113@gmail.com>
2025-07-19 16:42:55 -0400

    Fixes to run on render

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    host = '0.0.0.0' if os.environ.get("PORT") else '127.0.0.1'
+    app.run(host=host, port=port)
